4861451dbbdd2dc5dd3d0b2030bccf22409d9093
Antoine Lebrun <2148181@cegepmontpetit.ca>
2025-11-13 15:20:23 -0500

    Adds DTOs and Authentication Controller
    adding the DTOs and Authentication Controller to set up the authentication part of the app

---
+++ PersonalFinanceTracker/Models/DTOs/UserLoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace PersonalFinanceTracker.Models.DTOs
+{
+    public class UserLoginDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
